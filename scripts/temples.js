821d950ee62a6c4e9330bbe5fa8a91a10f3c3191
piusmakafui06 <piusmakafui@gmail.com>
2026-09-24 21:37:09 +0100

    correction

--- scripts/temples.js
+++ scripts/temples.js
@@ -4,8 +4,8 @@ currentyear.innerHTML = today.getFullYear();
 
 document.getElementById("lastModified").innerHTML = `<span class="highlight">Last Modification ${document.lastModified}</span>`;
 
-const mainnav = document.querySelector(".list");
-const hambtn = document.querySelector("#hamburger");
+const mainnav = document.querySelector(".navigation");
+const hambtn = document.querySelector("#menu");
 
 hambtn.addEventListener("click", () => {
     mainnav.classList.toggle("show");
